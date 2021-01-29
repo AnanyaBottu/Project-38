ca622b6efdcc0ee06ba52c28f1e1f64414c582a8
AnanyaBottu <58173885+AnanyaBottu@users.noreply.github.com>
2021-01-29 15:29:12 +0530

    Add files via upload

---
+++ 40/CarRacingStage1.5-master/js/Form.js
@@ -0,0 +1,63 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h1');
+    this.title = createElement('h1');
+    this.reset=createButton('Reset');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+    this.title.hide();
+  }
+
+  display(){
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 120, 0);
+
+    this.input.position(displayWidth/2 - 150 , displayHeight/2 - 80);
+    this.button.position(displayWidth/2 + 30, displayHeight/2);
+    this.input.style("font-size","25px");
+    this.button.style("font-size","25px");
+    
+
+
+    this.reset.position(displayWidth-100,20);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2 - 70, displayHeight/4);
+    });
+
+
+    this.reset.mousePressed(()=>{
+database.ref("/").set({
+  gameState:0,
+  playerCount:0,
+  endline:0
+})
+
+
+
+/*player.updateCount(0)
+  game.update(0);
+*/
+
+
+
+
+})
+ }
+
+}
+
